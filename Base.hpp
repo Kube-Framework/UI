9a4c4fe6cd14211d01965ee11f8ee891c31970fb
Ptah <m.moinvaziri@gmail.com>
2022-11-26 02:25:29 +0100

    [Fix]

--- Base.hpp
+++ Base.hpp
@@ -421,7 +421,7 @@ namespace kF::UI
     {
         /** @brief Get an opaque type handle from a templated type */
         template<typename Type>
-        [[nodiscard]] static inline TypeHash Get(void) noexcept { return TypeHash { typeid(Type).hash_code() }; }
+        [[nodiscard]] static constexpr TypeHash Get(void) noexcept { return TypeHash { typeid(Type).hash_code() }; }
 
         std::size_t hash {};
 
